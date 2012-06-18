fa67c44f6617ab6a756f30e03604e44c67a56c39
Sachin Kumar <skrathore.ror@gmailcom>
2012-06-19 01:26:46 +0530

    initial draft

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def contact
+  end
+  def about
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbuntu::Application.config.secret_token = 'cfc29f092ded29e3b574961b21f04cbee03c796deca1ae6939d7ef21cbe2361ecb0a870e6bd410d1e4c76cbcbef351572de91789673d0e86ee4f1cdba2e74518'

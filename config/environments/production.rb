cf13ad4f14d7c7357e48013b3a4747b0161121c6
mKumar <mkumar.rails@gmail.com>
2012-07-14 17:22:06 +0530

    change production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Grepruby::Application.configure do
 
   # Refinery has set config.assets.initialize_on_precompile = false by default.
-  config.assets.initialize_on_precompile = false
+  #config.assets.initialize_on_precompile = false
 
   # Settings specified here will take precedence over those in config/application.rb
 

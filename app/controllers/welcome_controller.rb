56f367d93516dfeecf28d99357c47bd081d1e7d9
mKumar <mkumar.rails@gmail.com>
2012-08-11 17:44:44 +0530

    change in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,4 +3,10 @@ class WelcomeController < ApplicationController
   	@posts = Refinery::Blog::Post.all(:limit => 2)
   	#render :layout => false
   end
+
+
+  def google30aecb4f32189612
+  	render :layout => false
+  end
+
 end
